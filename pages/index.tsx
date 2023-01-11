a5e876046540f05ff643bc7b8c68e9e97e94a751
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-11 14:03:36 -0300

    add Card and images tod index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,37 @@
 import { Inter } from '@next/font/google'
 import { ShopLayout } from '../components/layouts'
-import { Typography } from '@mui/material'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { initialData } from '../database/products'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Enuentra los mejores productos en Teslo Shop'} >
-       <Typography variant='h1' component='h1' >Tienda</Typography>
-       <Typography variant='h2' sx={{mb:1}} >Todos los productos</Typography>
+      <Typography variant='h1' component='h1' >Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }} >Todos los productos</Typography>
+
+      <Grid container spacing={4} >
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug} >
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+
+                </CardActionArea>
+
+              </Card>
+
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
-  
+
 
   )
 }
