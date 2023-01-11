721ca63a962a83c88603501c9176d4496f269296
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-11 00:11:49 -0300

    adding Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import { Inter } from '@next/font/google'
+import { ShopLayout } from '../components/layouts'
+import { Typography } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>Teslo-Shop</h1>
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Enuentra los mejores productos en Teslo Shop'} >
+       <Typography variant='h1' component='h1' >Tienda</Typography>
+       <Typography variant='h2' sx={{mb:1}} >Todos los productos</Typography>
+    </ShopLayout>
+  
 
   )
 }
