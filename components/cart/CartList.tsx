4bdef3b6ac2d70e2976782f8d5f3cd69885e636b
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-11 21:49:31 -0300

    add CartList.tsx

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material"
+import { Grid, Link, Typography, CardActionArea, CardMedia, Box, Button } from '@mui/material';
 import { initialData } from "../../database/products"
+import NextLink from 'next/link'
+import { ItemCounter } from '../ui/ItemCounter';
 
 interface Props {
 
@@ -19,7 +21,37 @@ export const CartList = () => {
         <>
             {
                 productsInCart.map(product => {
-                    <Typography key={product.slug} >{product.title}</Typography>
+                    <Grid container key={product.slug} spacing={2} sx={{ mb: 1 }} >
+                        <Grid item xs={3} >
+                            {/* TODO: llevar a la pagina del producto */}
+                            <NextLink href='/product/slug' passHref legacyBehavior >
+                                <Link>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            image={`products/${product.images[0]}`}
+                                            component='img'
+                                            sx={{ borderRadius: '5px' }}
+                                        />
+                                    </CardActionArea>
+                                </Link>
+                            </NextLink>
+                        </Grid>
+                        <Grid item xs={7} >
+                            <Box display='flex' flexDirection='column' >
+                                <Typography variant='body1' >{product.title}</Typography>
+                                <Typography variant='body1' >Talla: <strong>M</strong></Typography>
+                                {/* Condicional */}
+                                <ItemCounter />
+                            </Box>
+                        </Grid>
+                        <Grid item xs={5} display='flex' alignItems='center' flexDirection='column' >
+                            <Typography variant='subtitle1' >{`$${product.price}`}</Typography>
+                            {/* Editable */}
+                            <Button variant='text' color='secondary' >
+                                Remover
+                            </Button>
+                        </Grid>
+                    </Grid>
 
                 })
             }
