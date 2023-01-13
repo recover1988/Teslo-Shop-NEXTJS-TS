ca9f527a56721404eaa779f011866ccc898f08f4
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-12 21:57:58 -0300

    add useSWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
 import { ShopLayout } from '../components/layouts'
 import { Typography } from '@mui/material'
-import { initialData } from '../database/products'
 import { ProductList } from '../components/products/ProductList';
 
+import useSWR from 'swr'
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
 
-export default function Home() {
+export default function HomePage() {
+
+  const { data, error, isLoading } = useSWR('/api/products', fetcher) // se puede poner useSWR('/api/products', fetcher,{refreshInterval:1000}) para que refresque cada 1 seg
+  
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Enuentra los mejores productos en Teslo Shop'} >
       <Typography variant='h1' component='h1' >Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }} >Todos los productos</Typography>
 
-      <ProductList products={initialData.products as any} />
+      <ProductList products={data} />
 
     </ShopLayout>
 
