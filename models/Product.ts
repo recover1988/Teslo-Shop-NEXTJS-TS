4fb897ca1b6a8ad7fc50b50cc07ce268045db8e8
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-12 21:04:02 -0300

    add search by title and tag

--- models/Product.ts
+++ models/Product.ts
@@ -35,7 +35,7 @@ const productSchema = new Schema({
 })
 
 
-//TODO: Crear Indice de mongo
+productSchema.index({ title: 'text', tags: 'text' })
 
 const Product: Model<IProduct> = mongoose.models.Product || model('Product', productSchema)
 
