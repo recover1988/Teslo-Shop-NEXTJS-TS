87eeace73facdc94c35583989c683a0cbdd10955
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-15 19:55:14 -0300

    add validation of email

--- pages/api/user/register.ts
+++ pages/api/user/register.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { db } from '../../../database'
 import { User } from '../../../models'
 import bcrypt from 'bcryptjs';
-import { jwt } from '../../../utils';
+import { jwt, validations } from '../../../utils';
 
 type Data =
     | { message: string }
@@ -42,7 +42,9 @@ const registerUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => 
         return res.status(400).json({ message: 'El nombre debe de ser de 2 carateres' })
     }
     //TODO: validar email
-    // if(email){}
+    if (!validations.isValidEmail(email)) {
+        return res.status(400).json({ message: 'El correo no tiene el formato de correo' })
+    }
 
     const newUser = new User({
         email: email.toLowerCase(),
