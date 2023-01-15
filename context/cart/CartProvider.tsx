c778f8932bdedcc1a77270b005d47db6c8f83959
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-14 22:52:22 -0300

    orderSummary(subtotal,tax,total)

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -36,6 +36,21 @@ export const CartProvider: FC<Props> = ({ children }) => {
     }, [state.cart])
 
 
+    useEffect(() => {
+        const numberOfItems = state.cart.reduce((prev, current) => current.quantity + prev, 0)
+        const subTotal = state.cart.reduce((prev, current) => (current.quantity * current.price) + prev, 0)
+        const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0)
+
+        const orderSummary = {
+            numberOfItems,
+            subTotal,
+            tax: subTotal * taxRate,
+            total: subTotal * (taxRate + 1)
+        }
+
+        Cookie.set('cart', JSON.stringify(state.cart))
+    }, [state.cart])
+
     const addProductToCart = (product: ICartProduct) => {
         //! NIVEL 1
         // dispatch({type: '[Cart] - Add Product' , payload: product})
