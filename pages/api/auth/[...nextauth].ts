423079619ce84773660d8ed289c6c63689bc3b87
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-19 23:04:58 -0300

    add Credentials for our auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,34 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import Credentials from 'next-auth/providers/credentials'
 export const authOptions = {
   // Configure one or more authentication providers
+  // Para cambiar el orden simplemente movemos la credenciales de orden
   providers: [
+    // ...add more providers here
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: { lable: 'Correo:', type: 'email', placeholder: 'correo@google.com' },
+        password: { lable: 'Contraseña:', type: 'password', placeholder: 'Contraseña' },
+      },
+      //esta funcion tiene que devolver un null si falla o un objeto si esta todo bien
+      async authorize(credentials, req) {
+        //todo: validar contra base de datos
+        return null
+      }
+    }),
+
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // ...add more providers here
+
   ],
+
+  // Callbacks
+  callbacks: {
+
+  }
 }
 export default NextAuth(authOptions)
