9f602667b98a06f26a705b1dc9b889094ae01318
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-22 20:26:30 -0300

    add navitage to in the sideMenu

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -76,7 +76,7 @@ export const SideMenu = () => {
                                     <ListItemText primary={'Perfil'} />
                                 </ListItemButton>
 
-                                <ListItemButton>
+                                <ListItemButton onClick={() => navigateTo('/orders/history')}>
                                     <ListItemIcon>
                                         <ConfirmationNumberOutlined />
                                     </ListItemIcon>
