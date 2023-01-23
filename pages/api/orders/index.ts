dee09b4ec14660296dcaf5b96387735918e4596a
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-23 18:49:39 -0300

    round to 2 decimal the total

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -53,7 +53,11 @@ async function createOrder(req: NextApiRequest, res: NextApiResponse<Data>) {
             throw new Error('Total no cuadra con el monto')
         }
         const userId = session.user!._id
-        const newOrder = new Order({ ...req.body, isPaid: false, user: userId })
+        const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+
+        newOrder.total = Math.round( newOrder.total * 100) / 100;
+
+
         await newOrder.save();
         await db.disconnect()
         return res.status(200).json(newOrder)
