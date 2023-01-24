65db397feb59384b0303303e02ac53435ebf986d
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-23 21:44:23 -0300

    verify Order isPaid and transactionId

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -2,6 +2,9 @@
 
 import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { IPaypal } from '../../../interfaces'
+import { db } from '../../../database'
+import { Order } from '../../../models'
 
 type Data = {
     message: string
@@ -53,6 +56,9 @@ const getPaypalBearerToken = async (): Promise<string | null> => {
 
 const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
+// TODO: validar session del usuario
+//TODO: validar mongoId
+
     const paypalBearerToken = await getPaypalBearerToken();
 
     if (!paypalBearerToken) {
@@ -61,13 +67,34 @@ const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     const { transactionId = '', orderId = '' } = req.body
 
-    const { data } = await axios.get(`${process.env.PAYPAL_ORDERS_URL}/${transactionId}`,{
-        headers:{
+    const { data } = await axios.get<IPaypal.PaypalOrderStatusResponse>(`${process.env.PAYPAL_ORDERS_URL}/${transactionId}`, {
+        headers: {
             'Authorization': `Bearer ${paypalBearerToken}`
         }
     })
+    if (data.status !== 'COMPLETED') {
+        return res.status(401).json({ message: 'Orden no reconocida' })
+    }
+
+    await db.connect();
+    const dbOrder = await Order.findById(orderId);
+
+    if (!dbOrder) {
+        await db.disconnect();
+        return res.status(400).json({ message: 'Orden no existe en nuestra base de datos' })
+    }
+
+    if (dbOrder.total !== Number(data.purchase_units[0].amount.value)) {
+        await db.disconnect();
+        return res.status(400).json({ message: 'Los montos de Paypal y nuestra orden no son iguales' })
+    }
+
+    dbOrder.transactionId = transactionId;
+    dbOrder.isPaid = true;
+
+    await dbOrder.save()
+    await db.disconnect()
 
 
-
-    return res.status(200).json({ message: paypalBearerToken })
+    return res.status(200).json({ message: 'Orden pagada' })
 }
