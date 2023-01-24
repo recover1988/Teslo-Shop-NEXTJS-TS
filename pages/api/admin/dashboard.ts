0d8ebceadcffe61d6666269146e4a3bd3ba8602c
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-24 18:39:58 -0300

    add Promise.all

--- pages/api/admin/dashboard.ts
+++ pages/api/admin/dashboard.ts
@@ -13,25 +13,42 @@ type Data = {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
     await db.connect();
-    
-    const numberOfOrders = await Order.countDocuments({});
-    const paidOrders = await Order.countDocuments({ isPaid: true });
-    const notPaidOrders = await Order.countDocuments({ isPaid: false });
-    const numberOfClients = await User.countDocuments({ role: 'client' });
-    const numberOfProducts = await Product.countDocuments({});
-    const productsWithNoInventory = await Product.countDocuments({ inStock: 0 });
-    const lowInventory = await Product.countDocuments({ inStock: { $gt: 0, $lt: 10 } })
+
+    // const numberOfOrders = await Order.countDocuments({});
+    // const paidOrders = await Order.countDocuments({ isPaid: true });
+    // const numberOfClients = await User.countDocuments({ role: 'client' });
+    // const numberOfProducts = await Product.countDocuments({});
+    // const productsWithNoInventory = await Product.countDocuments({ inStock: 0 });
+    // const lowInventory = await Product.countDocuments({ inStock: { $lte: 10 } });
+
+
+    const [
+        numberOfOrders,
+        paidOrders,
+        numberOfClients,
+        numberOfProducts,
+        productsWithNoInventory,
+        lowInventory,
+    ] = await Promise.all([
+        Order.countDocuments({}),
+        Order.countDocuments({ isPaid: true }),
+        User.countDocuments({ role: 'client' }),
+        Product.countDocuments({}),
+        Product.countDocuments({ inStock: 0 }),
+        Product.countDocuments({ inStock: { $lte: 10 } }),
+    ])
 
     await db.disconnect();
 
     res.status(200).json({
         numberOfOrders,
         paidOrders,
-        notPaidOrders,
         numberOfClients,
         numberOfProducts,
         productsWithNoInventory,
         lowInventory,
+        notPaidOrders: numberOfOrders - paidOrders,
     })
 }
