43b6f734e33193fedddbf2e3375e35b66b253d45
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-23 22:53:04 -0300

    delete Orders from DB

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { db, seedDatabase } from '../../database';
-import { Product, User } from '../../models';
+import { Order, Product, User } from '../../models';
 
 type Data = {
     message?: string
@@ -16,6 +16,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     await Product.insertMany(seedDatabase.initialData.products);
     await User.deleteMany();
     await User.insertMany(seedDatabase.initialData.users);
+
+    await Order.deleteMany();
 
     await db.disconnect();
 
