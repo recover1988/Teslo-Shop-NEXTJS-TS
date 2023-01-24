5dedd8100b2bc3d8a657a1a6a250fe264dfba2cc
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-23 21:17:35 -0300

    generate token of verification paypal

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,5 +1,6 @@
 
 
+import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -20,10 +21,42 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 
 }
 
-const payOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+const getPaypalBearerToken = async (): Promise<string | null> => {
 
-    
+    const PAYPAL_CLIENT = process.env.NEXT_PUBLIC_PAYPAL_CLIENT;
+    const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
 
 
-     return res.status(200).json({ message: 'Orden pagada' })
+    const base64Token = Buffer.from(`${PAYPAL_CLIENT}:${PAYPAL_SECRET}`, 'utf-8').toString('base64')
+    const body = new URLSearchParams('grant_type=client_credentials')
+
+    try {
+        const { data } = await axios.post(process.env.PAYPAL_OAUTH_URL || '', body, {
+            headers: {
+                'Authorization': `Basic ${base64Token}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        })
+
+        return data.access_token;
+
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.log(error.response?.data)
+        } else {
+            console.log(error)
+        }
+        return null
+    }
+}
+
+
+const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const paypalBearerToken = await getPaypalBearerToken()
+    if (!paypalBearerToken) {
+        return res.status(400).json({ message: 'No se pudo confirmar el token de paypal' })
+    }
+
+    return res.status(200).json({ message: paypalBearerToken })
 }
